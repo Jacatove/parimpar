51541b9f2943a877ef65bd4006acb4f2c614905b
Javier <javiercamilotorresver@gmail.com>
2021-02-05 04:26:09 +0000

    se agrega docstring a función parimpar

--- lib.py
+++ lib.py
@@ -1,5 +1,14 @@
 
 def parimpar(num):
+    """Esta función determina si un número entero es par o impar
+
+    input:
+        num (entero)
+
+    output:
+        'par' (str)
+        'impar' (str)
+    """
     if num%2 == 0:
         return 'par'
     else:
